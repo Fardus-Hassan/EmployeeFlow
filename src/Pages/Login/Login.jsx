4ff5f1d761ac12a36ec36f00b040eb5272132b05
Fardus-Hassan <webdev.fardus@gmail.com>
2024-06-06 21:34:04 +0600

    added a register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-themeColor2 lg:max-w-4xl">
-                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}></div>
+                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://images.pexels.com/photos/6415/apple-desk-office-working.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" }}></div>
 
                 <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
                     <div className='flex items-center justify-center gap-3 mb-5'>

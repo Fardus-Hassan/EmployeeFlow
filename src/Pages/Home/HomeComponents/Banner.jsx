1a9edd95949ba0aa83235316b9bffb21e85e3aa3
Fardus-Hassan <webdev.fardus@gmail.com>
2024-06-15 00:13:40 +0600

    added parallux like effect in banner

--- src/Pages/Home/HomeComponents/Banner.jsx
+++ src/Pages/Home/HomeComponents/Banner.jsx
@@ -1,28 +1,55 @@
+import { useEffect, useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 
 const Banner = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const position = window.scrollY;
+            setScrollPosition(position);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
-
-            <div className="w-full h-[90vh] relative bg-fixed bg-no-repeat bg-cover bg-center" style={{backgroundImage: `url("https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`}}>
-                {/* <img className="w-full h-[80vh] object-cover" src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
-                <div className="bg-black bg-opacity-50 w-full h-full absolute top-0 flex justify-center items-center">
-                   <div className="w-[95%] mx-auto">
-                   <h1 className=' max-w-[600px] mx-auto text-center text-white lg:text-[46px] text-[35px] font-black font-poppins'>Empowering Workforce <span className='text-pmColor'><Typewriter
-                        words={['Productivity', 'Performance', 'Effectiveness']}
-                        typeSpeed={100}
-                        deleteSpeed={50}
-                        delaySpeed={3000}
-                        cursorStyle='_'
-                        autoStart={true}
-                        loop={true}
-                        cursor='pointer'
-                    /></span></h1>
-                    <p className='max-w-[800px] w-[95%] mx-auto text-center font-medium text-white lg:text-[16px] text-[14px] mt-4 mb-8 font-montserrat'>Streamline HR tasks, boost productivity, and foster collaboration with our innovative workforce management solutions. Join us for efficiency.</p>
-                   </div>
+            <div className="w-full h-[95vh] relative bg-fixed bg-no-repeat bg-cover bg-center transition-transform duration-500 ease-out" style={{
+                backgroundImage: `url("https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+                // transform: `translateY(${scrollPosition / -15}px)`
+            }}>
+                <div className="bg-black bg-opacity-50 w-full h-full absolute top-0 flex justify-center items-center transition-opacity duration-300 ease-in-out">
+                    <div className="w-[95%] mx-auto transition-transform duration-500 ease-out" style={{
+                        // transform: `translateY(${scrollPosition / -1.5}px)`
+                    }}>
+                        <h1 className={`mx-auto text-center text-white overflow-hidden lg:text-[46px] text-[35px] font-black font-poppins`}> <span className="sm:flex sm:justify-center sm:items-center gap-5"><span className={`duration-100 ease-out block`} style={{
+                                transform: `translateX(${scrollPosition / -1}px)`
+                            }}>Empowering</span> <span
+                            className={`duration-100 ease-out block`} style={{
+                                transform: `translateX(${scrollPosition / 1}px)`
+                            }}>Workforce</span> </span>
+                            <span className={`text-pmColor duration-100 ease-out block`}  style={{
+                                transform: `translateY(${scrollPosition / -5}px)`
+                            }}><Typewriter
+                            words={['Productivity', 'Performance', 'Effectiveness']}
+                            typeSpeed={100}
+                            deleteSpeed={50}
+                            delaySpeed={3000}
+                            cursorStyle='_'
+                            autoStart={true}
+                            loop={true}
+                            cursor='pointer'
+                        /></span></h1>
+                        <p className='max-w-[800px] w-[95%] mx-auto text-center duration-100 ease-out block font-medium text-white lg:text-[16px] text-[14px] mt-4 mb-8 font-montserrat' style={{
+                                transform: `translateY(${scrollPosition / 5}px)`
+                            }}>Streamline HR tasks, boost productivity, and foster collaboration with our innovative workforce management solutions. Join us for efficiency.</p>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
--- src/Pages/Home/HomeComponents/Services.jsx
+++ src/Pages/Home/HomeComponents/Services.jsx
@@ -2,7 +2,38 @@
 
 const Services = () => {
 
-    const items = [1, 2, 3, 4, 5, 6]
+    const items = [
+        {
+            image: "https://images.pexels.com/photos/20639510/pexels-photo-20639510/free-photo-of-electronic-devices-in-an-office.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Attendance Tracking",
+            description: "Effortless employee attendance monitoring and reporting."
+          },
+          {
+            image: "https://www.hono.ai/public/uploads/images/202405/image_750x415_1716859798_791b4161a40601501d66.jpg",
+            title: "Performance Evaluation",
+            description: "Comprehensive employee performance reviews and feedback."
+          },
+          {
+            image: "https://images.pexels.com/photos/6694543/pexels-photo-6694543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Payroll Management",
+            description: "Streamlined payroll processing and tax management."
+          },
+          {
+            image: "https://www.orangehrm.com/assets/Uploads/Leave-Time-Off-Management-System.jpg",
+            title: "Leave Management",
+            description: "Efficient tracking and management of employee leaves."
+          },
+          {
+            image: "https://images.pexels.com/photos/4344860/pexels-photo-4344860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "HR Analytics",
+            description: "Insightful data-driven analytics for better HR decision-making."
+          },
+          {
+            image: "https://images.pexels.com/photos/7709242/pexels-photo-7709242.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Employee Self-Service",
+            description: "Empower employees with self-service options for HR tasks."
+          }
+    ]
 
     return (
         <div className='sm:my-[100px] my-16 w-[95%] mx-auto'>
@@ -16,11 +47,11 @@ const Services = () => {
                 {
                     items.map((item, index) => (
                         <div key={index} className="w-full overflow-hidden bg-white rounded-lg shadow-lg dark:bg-themeColor2">
-                            <img className="object-cover w-full h-56" src="https://images.pexels.com/photos/4050315/pexels-photo-4050315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" />
+                            <img className="object-cover object-top w-full h-[300px]" src={item.image} alt="avatar" />
 
                             <div className="text-center pb-5">
-                                <a href="#" className="block text-xl font-bold text-gray-800 dark:text-white py-5" tabindex="0" role="link">Seamless Onboarding and Smooth Exits</a>
-                                <span className="text-sm text-gray-700 dark:text-gray-200"> Ensure efficient employee transitions with automated checklists, document management, and streamlined onboarding and offboarding processes.</span>
+                                <a href="#" className="block text-xl font-bold text-gray-800 dark:text-white pt-5"role="link">{item.title}</a>
+                                <span className="text-sm text-gray-700 dark:text-gray-200 block pt-3">{item.description}</span>
                             </div>
                         </div>
                     ))
